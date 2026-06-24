b1d9024a40f911d9fe8d3cc0918d35f3ebb53143
VamsiKrishnag549 <vamsikrishnagokaraju123@gmail.com>
2026-06-24 11:48:29 +0530

    Update SecurityConfig.java

--- src/main/java/com/nt/security/SecurityConfig.java
+++ src/main/java/com/nt/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
             throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
